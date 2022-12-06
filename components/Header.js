745e896e48b3395fe2ff554b736de87bb0005c7a
Gayathri R N <gayathri.9mar94@gmail.com>
2022-12-06 21:00:39 +0530

    fixes

--- components/Header.js
+++ components/Header.js
@@ -1,5 +1,5 @@
 import React from "react";
-import reactImg from "../Images/subheading.gif";
+import reactImg from "../Images/Subheading.gif";
 import Image from "next/image";
 import HeaderItems from "./HeaderItems";
 import {
