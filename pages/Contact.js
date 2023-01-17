12632b622b181e22cce94ffe4ce792749e945d24
Gayathri R N <gayathri.9mar94@gmail.com>
2023-01-17 13:03:55 +0530

    fixes

--- pages/Contact.js
+++ pages/Contact.js
@@ -10,7 +10,6 @@ function Contact() {
       <Header />
       <dl className="m-10 overflow-y-scroll max-h-screen pb-80 md:max-h-screen sm:grid md:grid-cols-2 justify-items-center items-center justify-center scrollbar-hide">
         <div className="bg-gradient-to-r from-cyan-500 to-blue-500 px-4 py-5 gap-4 m-5 shadow rounded-2xl h-80 flex justify-around items-center min-w-[50%]">
-          {/* <dt className="text-lg font-bold text-gray-900">Mail to:</dt> */}
           <dd className="mt-1 text-sm text-gray-900 sm:col-span-2 sm:mt-0 cursor-pointer">
             <a
               href="mailto:gayathri.9mar94@gmail.com"
